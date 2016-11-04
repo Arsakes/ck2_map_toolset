f94a4aa3f8f7d9cd4d2fc7c252b53273221bdeff
pietrko <elminister@interia.pl>
2016-11-04 21:41:16 +0100

    Aktualizacja.

--- mapLib.py
+++ mapLib.py
@@ -9,9 +9,9 @@ import numpy as np
 def updateAdj():
   adjIn = 'adjacencies.csv'
   adjOut = 'adjacenciesNEW.csv'
-  filename = 'definitionNEW.csv'
+  filename = 'definition.csv'
   # create id list
-  with open(filename, 'rb') as csvSrc:
+  with open(filename, 'r') as csvSrc:
     base = csv.reader(csvSrc, delimiter=';')
     idList = set()
     n = 0
@@ -19,19 +19,19 @@ def updateAdj():
       if (n != 0):
         idList.add(int(row[0]))
       n = 1
-
-  with open(adjIn, 'rb') as csvAdj:
-    out = open(adjOut, 'wb')
+ 
+  with open(adjIn, 'r') as csvAdj:
+    out = open(adjOut, 'w')
     base = csv.reader(csvAdj, delimiter=';')
     csvOut = csv.writer(out, delimiter=';')
     # check from and To
     first = True
     for row in base:
-      if ( first == False):
+      if (first == False):
         idFrom = int(row[0])
         idTo = int(row[1])
-        if(idFrom in idList or idTo in idList):
-          print ("yes")
+        if(idFrom in idList and idTo in idList):
+          print ("Provs: " + idFrom + ' and ' + idTo + ' joined')
           csvOut.writerow(row)
         else:
           print ("not")
@@ -40,7 +40,7 @@ def updateAdj():
         csvOut.writerow(row)
 
 
-# Class for provinces.bmp handling
+# Class for provinces.bmp handling, reads out the information from image 
 #
 class ProvincesBMP:
   
@@ -52,6 +52,21 @@ class ProvincesBMP:
 
     else:
       self.data = Image.open(filename)
+
+  def checkMissing(self, provinceData):
+    """Checks if there is any color from the bmp map not on the the list"""
+    if (self.colorset):
+      self.getColors()
+
+    pColors = set()
+    for province in provinceData.data:
+      color = provinceData.getColor(province)
+      pColors.add(color)
+
+    print('COLORS ON BMP:', len(self.colorset))
+    for color in self.colorset:
+      if (not(color in pColors)):
+        print('No province for color: ', color)
 
   def getColors(self, filename=None):
     """Creates a unique table of colors from bmp image"""
@@ -135,9 +150,6 @@ class ProvincesBMP:
     self.data.save(filename)
 
 
-#
-# Class for handling positions file
-#
 
 # Main function fixes the csv file that mapfiller tool uses
 # for generation
@@ -179,12 +191,17 @@ class ProvincesData:
     outdata.writeheader();
     outdata.writerows(self.data)
     csvOUT.close()
-      
+     
+
   def _recomputeId(self):
     '''Private function should be executed each time provinces are changed to recompute ids'''
     n=1
+    self.idMap = {}
     for province in self.data:
+      oldId = int(province['Province ID'])
       province['Province ID'] = n
+      # for future reference keep id map
+      self.idMap[oldId] = n
       n=n+1
  
 
@@ -205,12 +222,12 @@ class ProvincesData:
 
   def countProvinces(self):
     '''Count the amounts of provinces currently in the file'''
+    print('Amount of provinces:', len(self.data))
     return len(self.data)
 
 
   def getColor(self, province):
     '''Return tuple with province color'''
-    print(province['Red'])
     red = int(province['Red'])
     green = int(province['Green'])
     blue = int(province['Blue'])
@@ -244,6 +261,46 @@ class ProvincesData:
     self.data = temp
 
 
+  def updateAdj(self):
+    '''Updates adjacencies file'''
+    adjIn = 'adjacencies.csv'
+    adjOut = 'adjacenciesNEW.csv'
+
+    oldIdSet = set()
+    # create id list
+    if (self.idMap == {}):
+      print("ERROR")
+    else:
+      oldIdList = set(self.idMap.keys())
+ 
+    # Open old adjacencies
+    with open(adjIn, 'r') as csvAdj:
+      out = open(adjOut, 'w')
+      base = csv.reader(csvAdj, delimiter=';')
+      csvOut = csv.writer(out, delimiter=';')
+      # check from and To
+      first = True
+      for row in base:
+        if (first == False):
+          idFrom = int(row[0])
+          idTo = int(row[1])
+
+          # Check in old id set
+          if(idFrom in oldIdList and idTo in oldIdList):
+            print ("Provs: " + str(idFrom) + ' and ' + str(idTo) + ' joined')
+            newIdFrom = self.idMap[idFrom]
+            newIdTo = self.idMap[idTo]
+            print ("Old ids: " + str(idFrom) + ", " + str(idTo) + " mapped to " + str(newIdFrom) + ", " + str(newIdTo))
+            row[0] = newIdFrom
+            row[1] = newIdTo
+            csvOut.writerow(row)
+          else:
+            print ("not")
+        else:
+          first = False 
+          csvOut.writerow(row)
+
+
   def generateProvSetup(self, filename):
     '''Since patch 2.5 generate province setup file'''
     text_file = open(filename, "w")
@@ -272,3 +329,12 @@ class ProvincesData:
 
     text_file.write(text)
     text_file.close() 
+
+
+
+# Positions file class
+# Postions are counted in following way:
+# positions.txt 
+# y+ = bottom to up
+# from left - to right x+
+# :
--- run.py
+++ run.py
@@ -1,22 +1,49 @@
-#!/bin/python2
+#!/bin/python
 from mapLib import *
+from grammar import *
 import pickle
 
 
-#colors = pickle.load(open('uniqueList', 'rb'))
-#createSubDefCSV(colors, 'definition.csv', 'definitionNEW.csv')
-
-#fixTheCSV(colors)
-
+# Firstly we construct province information using provinceDef.xls/csv
+# this is our base file we want to change!
 a=ProvincesData('provinceDef.csv')
-b=ProvincesBMP('provinces2.bmp')
+
+# In order to know in which way we want to change provinceDef.xls/csv
+# We need bmp image 
+b=ProvincesBMP('provinces_v2.bmp')
+
+# Get all colors (so provinces) exsiting in provinces_v2.bmp
 colors = b.getColors()
+
+# Use this info to delete all provs not existing in base provinceDef file
 a.filterByColor(colors)
-print(a.getUniqueValues('Culture'))
-#b.generatePropertyMap(a, 'Culture')
-a.write('provinceDef2.csv')
 
 
-#b.save('newprov.bmp')
-#b.getColors('newColors')
-#print(pickle.load(open('newColors','rb')))
+# Counting
+a.countProvinces()
+
+# Checking 
+b.checkMissing(a)
+
+# Write the new csv file
+a.write('provinceDef_v2.csv')
+# after this you should transform provinceDef_v2.csv  into XLS excel file and then use CK2Tools
+
+
+###########################################
+# 
+# Positions update
+#
+# Remember we have also positions.txt to update -> most likely we have changed the map geometry so adjustment is needed
+# This instruction takes 
+posdata = PositionsData('positions.txt')
+
+# Translate the positions
+posdata.move(-1579,-1162)
+
+# Use the province definitions to updated positions data 
+#(remember some provs are deleted from existence and we have OLD version of positions data)
+posdata.update(a)
+
+# Save updated positions data file
+posdata.write('posnew.txt')
